2e0d77547f308d6f0ea8c1a0945a5782540492b7
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-20 16:25:48 -0700

    Adding to questions array

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const gen = require('./utils/generateMarkdown.js');
+const inquirer = require(`inquirer`);
+const fs = require(`fs`);
+const gen = require(`./utils/generateMarkdown.js`);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    // TYPE:
+    // type: input, number, confirm, list, rawlist, expand, checkbox, password, editor
+
+    // CHOICES
+    type: `input`,
+    message: `Project title: `,
+    name: `project`,
+},{
+    type: `input`,
+    message: `Enter a description: `,
+    name: `description`,
+},
+{
+    type: `input`,
+    message: `Installation instructions: `,
+    name: `installation`,
+},
+{
+    type: `list`,
+    message: `Usage: `,
+    name: `usage`,
+},
+{
+    type: `choice`,
+    message: `Choose your license(Optional): `,
+    choices: [`None`,`MIT`, `Apache`, `Boost`, `CC0`],
+},
+];
+
+
+// [
+//     `Project Title`,
+//     `Description`,
+//     `Installation instructions`,
+//     `Usage Information`,
+//     `Contribution guidelines`,
+//     `test instructions`
+// ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName + '.md', process.argv[2], (err) =>
-    err ? console.error(err) : console.log('Success!')
-);
+    fs.writeFile(`${fileName}.md`, data, function(error) {
+        error ? `Error, something went wrong` : `Success`;
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile(x, y);
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  const licenses = {
-    apache: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-    gnu_gpl: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
-    mit: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-    bsd2: "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
-    bsd3: "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
-    boost: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
-    cc0: "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)"
-  }
+    const licenses = {
+        apache: `[![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+        gnu_gpl: `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+        mit: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+        bsd2: `[![License: BSD 2](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`,
+        bsd3: `[![License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`,
+        boost: `[![License: Boost](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`,
+        cc0: `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`
+    }
 
-  if (licenses.contains(license)) {
-    return licenses.license;
-  } else {
-    return "";
-  }
+    if (licenses.contains(license)) {
+        return licenses.license;
+    } else {
+        return ``;
+    }
 }
 
 // TODO: Create a function that returns the license link
@@ -32,9 +32,9 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  
-`;
+    return `# ${data.title}
+
+    `;
 }
 
 module.exports = generateMarkdown;
