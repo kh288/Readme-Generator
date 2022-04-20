236e8ed24772b023973a691a88e25d77c98b5c21
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-20 07:48:35 -0700

    Added instructions and setting up code

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const gen = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName + '.md', process.argv[2], (err) =>
+    err ? console.error(err) : console.log('Success!')
+);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
